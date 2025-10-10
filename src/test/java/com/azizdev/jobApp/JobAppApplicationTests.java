b27e6f67387aeb7417d0a04d065e31b1d57abe01
Aziz Ben Lamine <azizbenlamine@gmail.com>
2025-10-10 08:15:38 +0100

    Initial commit

---
+++ src/test/java/com/azizdev/jobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azizdev.jobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
