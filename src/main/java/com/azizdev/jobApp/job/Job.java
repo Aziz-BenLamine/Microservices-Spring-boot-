6899e0dfc3f259258c34a76876b423e2db28e5f1
Aziz Ben Lamine <azizbenlamine@gmail.com>
2025-10-11 08:30:57 +0100

    Add JPA dependencies and implement Job repository and service methods

--- src/main/java/com/azizdev/jobApp/job/Job.java
+++ src/main/java/com/azizdev/jobApp/job/Job.java
@@ -1,12 +1,20 @@
 package com.azizdev.jobApp.job;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String description;
     private String minSalary;
     private String maxSalary;
     private String location;
+
+    public Job() {}
 
     public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
         this.id = id;
@@ -65,3 +73,5 @@ public class Job {
         this.location = location;
     }
 }
+
+
